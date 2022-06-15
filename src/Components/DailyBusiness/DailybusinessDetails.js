3cd91abaa2cd1bca63761cefceb47b6a71e977c2
MAYINUDDIN <mainu768971@gmail.com>
2022-06-15 17:28:06 +0600

    data updated

--- src/Components/DailyBusiness/DailybusinessDetails.js
+++ src/Components/DailyBusiness/DailybusinessDetails.js
@@ -10,30 +10,6 @@ const DailybusinessDetails = () => {
 
     console.log(dailydata);
 
-    // useEffect(() => {
-    //     axios.get(`http://202.164.213.67/payment/daily_business.php?FROM_DATE=${from}&&TOO_DATE=${to}`)
-    //         .then(data => {
-    //             const loadedData = data.data;
-    //             const name = loadedData.OFF_NAME;
-    //             console.log(name);
-    //             console.log(loadedData);
-    //             const ekokScData = loadedData.map(ekok => {
-    //                 const div_name = ekok.OFF_NAME;
-    //                 const first_year = ekok.TOTAL_1ST_YR;
-    //                 const t_ren = ekok.TOTAL_RENEWAL;
-    //                 const total = ekok.TOTAL;
-    //                 <h3>{div_name}</h3>
-
-    //                 console.log(div_name, first_year, t_ren, total);
-    //                 // const partss = ekok.OFF_NAME;
-
-
-    //             });
-    //             SetDailyData(loadedData);
-    //             // console.log(ekokScData);
-    //         })
-    // }, [])
-
 
 
     useEffect(() => {
@@ -71,10 +47,10 @@ const DailybusinessDetails = () => {
     return (
         <div>
 
-            <div className="col-md-12 shadow p-1">
-                <button style={{ fontSize: '16px' }} className='btn rounded mb-2 btn-success drop-shadow-lg bg-[#087f23] btn-md text-white m-1'><b>DAILY BUSINESS REPORT <span className='text-fareast text-[#ffea00] text-lg'>From: {from} To: {to}</span>  </b></button>
+            <div className="col-md-12 shadow m-2 p-3">
+                <button style={{ fontSize: '20px' }} className='btn rounded mb-3 btn-success drop-shadow-lg bg-[#087f23] btn-md text-white m-1'><b>DAILY BUSINESS REPORT-FIRST YEAR <span className='text-fareast text-[#ffea00] text-lg'>(From: {from} To: {to})</span>  </b></button>
 
-                <div style={{ width: "100%", height: "280px", marginTop: '0px', background: '#fafafa' }}>
+                <div style={{ width: "100%", height: "480px", marginTop: '0px', background: '#fafafa' }}>
 
                     <ResponsiveContainer>
                         <ComposedChart
@@ -98,31 +74,31 @@ const DailybusinessDetails = () => {
                             />
 
                             <Tooltip />
-                            <Legend verticalAlign="top" align="left" height={150} />
+                            <Legend verticalAlign="top" align="left" height={350} />
 
-                            <Bar
+                            {/* <Bar
                                 radius={[0, 0, 0, 0]}
                                 dataKey="fy_Target"
-                                barSize={30}
+                                barSize={70}
                                 fill="#00766c"
                                 yAxisId="left"
                                 tick={{ fill: 'dark' }}
                                 legendType="rect"
                                 name="FY_Target"
                                 label={{ position: 'top' }}
-                            />
+                            /> */}
 
                             <Bar
-                                radius={[0, 0, 0, 0]}
+                                radius={[3, 3, 0, 0]}
                                 dataKey="fy"
-                                barSize={25}
-                                fill="#00b248"
+                                barSize={60}
+                                fill="#004c8c"
                                 yAxisId="left"
                                 legendType="rect"
-                                name="FY_Business"
+                                name="First Year Business"
                                 label={{ position: 'top' }}
                             />
-                            <Line
+                            {/* <Line
                                 dot={true}
                                 activeDot={{ r: 10 }}
                                 strokeWidth={3}
@@ -135,13 +111,116 @@ const DailybusinessDetails = () => {
                                 name='Achievement'
                                 legendType="rect"
 
-                            />
+                            /> */}
                             <YAxis
                                 tickLine={false}
                                 yAxisId="left"
                                 axisLine={{ stroke: "#f5f5f5" }}
-                                unit="C"
-                                tickCount={10}
+                                unit="k"
+                                tickCount={100}
+                            />
+                            <YAxis
+                                tickLine={false}
+                                yAxisId="right"
+                                orientation="right"
+                                stroke="#3B7AD9"
+                                axisLine={{ stroke: "#f5f5f5" }}
+                            // unit="K"
+                            // domain={[5, "dataMax + 5"]}
+                            // tickCount={5}
+                            />
+                        </ComposedChart>
+
+                    </ResponsiveContainer>
+
+                </div>
+
+                {/* 
+                <div className='flex justify-center justify-evenly px-10 mt-1'>
+                    {
+                        ekokData.map(product => <ZoneValue key={product.id} product={product}></ZoneValue>)
+
+                    }
+
+                </div> */}
+
+            </div>
+
+
+
+            {/* ================== */}
+
+            <div className="col-md-12 shadow m-2 p-3">
+                <button style={{ fontSize: '20px' }} className='btn rounded mb-3 btn-success drop-shadow-lg bg-[#087f23] btn-md text-white m-1'><b>DAILY BUSINESS REPORT-RENEWAL <span className='text-fareast text-[#ffea00] text-lg'>(From: {from} To: {to})</span>  </b></button>
+
+                <div style={{ width: "100%", height: "480px", marginTop: '0px', background: '#fafafa' }}>
+
+                    <ResponsiveContainer>
+                        <ComposedChart
+                            width={1000}
+                            height={500}
+                            data={dailydata}
+
+                            margin={{
+                                top: 0,
+                                right: 0,
+                                left: 0,
+                                bottom: 0
+                            }}
+                        >
+
+                            <XAxis
+                                tick={{ fill: 'dark' }}
+                                dataKey="name"
+                                tickLine={false}
+                                axisLine={{ stroke: "#333" }}
+                            />
+
+                            <Tooltip />
+                            <Legend verticalAlign="top" align="left" height={350} />
+
+                            {/* <Bar
+                                radius={[0, 0, 0, 0]}
+                                dataKey="fy_Target"
+                                barSize={70}
+                                fill="#00766c"
+                                yAxisId="left"
+                                tick={{ fill: 'dark' }}
+                                legendType="rect"
+                                name="FY_Target"
+                                label={{ position: 'top' }}
+                            /> */}
+
+                            <Bar
+                                radius={[3, 3, 0, 0]}
+                                dataKey="re"
+                                barSize={60}
+                                fill="#00b686"
+                                yAxisId="left"
+                                legendType="rect"
+                                name="Renewal Business"
+                                label={{ position: 'top' }}
+                            />
+                            {/* <Line
+                                dot={true}
+                                activeDot={{ r: 10 }}
+                                strokeWidth={3}
+                                strokeLinecap="round"
+                                type="monotone"
+                                dataKey="fy_ratio"
+                                stroke="#ffab40"
+                                yAxisId="right"
+                                color='#fff'
+                                name='Achievement'
+                                legendType="rect"
+
+                            /> */}
+                            <YAxis
+                                tickLine={false}
+                                yAxisId="left"
+                                axisLine={{ stroke: "#f5f5f5" }}
+                                unit="k"
+                                tickCount={100}
                             />
                             <YAxis
                                 tickLine={false}
